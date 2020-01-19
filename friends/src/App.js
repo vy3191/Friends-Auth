553aeb7153fc43a777581cbfd664c4618df34582
vy3191 <venkyafhs@gmail.com>
2020-01-18 19:07:08 -0500

    Loader is working now

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Login from './components/Login';
 import Home from './components/Home';
 import PageNotFound from './components/PageNotFound';
-import Loader from './components/Loader';
+import Loading from './components/Loading';
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import './App.css';
 
@@ -15,9 +15,10 @@ function App() {
       <Router>
           <div>
            <Switch>
+             <Route exact path="/" component={Login} />
              <Route path="/login" component={Login}/>  
              <Route path="/logout" component={Login} />
-             <Route path="/loading" component={Loader} />
+             <Route path="/loading" component={Loading} />
              <Route path="/home-page" component={Home} />
              <Route component={Login} />
            </Switch>
